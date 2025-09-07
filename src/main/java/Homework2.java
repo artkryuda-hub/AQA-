a3cde6438b2304dbc86b50bea475c3ffd747a341
artkryuda-hub <artkryuda@gmail.com>
2025-09-07 22:41:36 +0300

    feat: complete task 1 homework 1

---
+++ src/main/java/Homework2.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Домашнее задание");
+        System.out.println("1 - Задание №1");
+        System.out.println("2 - Задание №2");
+        System.out.println("3 - Задание №3)");
+        System.out.println("4 - Задание №4");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1: pointCheck(scanner); break;
+            case 2: exitLoop(scanner); break;
+            case 3: sumCalculation(scanner); break;
+            case 4: tvControlRemote(scanner); break;
+            default: System.out.println("Неверный выбор");
+        }
+
+        scanner.close();
+    }
+    // Задание 1
+    private static void pointCheck(Scanner scanner){
+        int score;
+        while (true) {
+            System.out.print("Please enter your score (0-100): ");
+            score = scanner.nextInt();
+
+            if (score >= 0 && score <= 100) {
+                break;
+            } else {
+                System.out.println("Введено некорректное значение! Попробуйте снова.");
+            }
+        }
+
+        if (score >= 90) {
+            System.out.println("Your grade is A!");
+        } else if (score >= 80) {
+            System.out.println("Your grade is B!");
+        } else if (score >= 70) {
+            System.out.println("Your grade is C!");
+        } else if (score >= 60) {
+            System.out.println("Your grade is D!");
+        } else if (score >= 50) {
+            System.out.println("Your grade is E!");
+        } else {
+            System.out.println("Your grade is F!");
+        }
+    }
+    private static void exitLoop(Scanner scanner) {
+        System.out.println("Задание 2");
+    }
+
+    private static void sumCalculation(Scanner scanner) {
+        System.out.println("Задание 3");
+    }
+
+    private static void tvControlRemote(Scanner scanner) {
+        System.out.println("Задание 4");
+    }
+}
+
